fd0265b16f59405094e8f11be038ccf003964176
Alex Nunes <alex.et.nunes@gmail.com>
2016-04-11 14:27:22 -0300

    Minor changes

--- common_python/geojson.py
+++ common_python/geojson.py
@@ -3,10 +3,9 @@ import datetime
 import simplejson as json
 import common_python.compress as cp
 from library import pg_connection as pg
-import Jinja2 as jinja
 
 
-d = open('common_python/datadirectory.txt', 'r')
+d = open('datadirectory.txt', 'r')
 d = d.readline().splitlines()
 DATADIRECTORY = d[0]
 
@@ -35,7 +34,7 @@ def get_station_locations(station, table):
     return location
 
 
-def create_geojson(detections, dets_table=''):
+def create_geojson(detections, dets_table='', inc=5000):
     # Create a DataFrame from the CSV
     full_path_detections = "%s%s" % (DATADIRECTORY, detections)
     dets = pd.read_csv(full_path_detections)
@@ -51,8 +50,10 @@ def create_geojson(detections, dets_table=''):
         dets_table = full_path_detections.lower().replace('_compressed_detections', '').replace(DATADIRECTORY, '').replace('.csv', '')
 
     locs = get_station_locations(dets.station.unique().astype(str), dets_table)
+    locs = locs.drop_duplicates(subset='station')
 
     data = pd.merge(locs, dets, on='station', how='inner')
+
     data['startdate'] = data['startdate'].map(unix_time_millis)
     data['enddate'] = data['enddate'].map(unix_time_millis)
     data = data.sort_values(by='startdate', ascending=True)
@@ -66,11 +67,9 @@ def create_geojson(detections, dets_table=''):
     animals = animals.set_index(['animals'])
     animals = animals.T.to_dict()
 
-    inc = 5000
     detection_geojson = []
     start = 1
     end = inc
-
 
     while start < data.index.size:
         geojson ={
@@ -89,10 +88,9 @@ def create_geojson(detections, dets_table=''):
                     'hue': str(animals[d['catalognumber']]['hue'])
                 } for index, d in data[start:end].iterrows()]
         }
-
         detection_geojson.append(geojson)
-        start = end
-        end += inc
+        start = end+1
+        end = end + inc
 
 
     json_name = full_path_detections.lower().replace('.csv', '')
@@ -103,5 +101,5 @@ def create_geojson(detections, dets_table=''):
     return detection_geojson
 
 def create_html():
-    
+
     return html
