07802fd8369c2b9100e2a04ce04c25b1dae32336
Alex Nunes <alex.et.nunes@gmail.com>
2016-04-26 14:39:37 -0300

    Comments and Notebook added to all files

--- common_python/geojson.py
+++ common_python/geojson.py
@@ -13,6 +13,15 @@ d = d.readline().splitlines()
 DATADIRECTORY = d[0]
 
 
+'''
+unix_time_millis(dt)
+-------------------
+Returns a datetime in milliseconds
+
+@var dt - datetime/timestamp
+'''
+
+
 def unix_time_millis(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     date = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
@@ -55,6 +64,7 @@ def create_geojson(detections, dets_table='', inc=5000):
     full_path_detections = "%s%s" % (DATADIRECTORY, detections)
     dets = pd.read_csv(full_path_detections)
 
+    # determine if the file needs to be compressed
     if not (set(['startdate', 'enddate', 'station']).issubset(dets.columns)):
         full_path_detections = cp.CompressDetections(detections)
         dets = pd.read_csv(full_path_detections)
@@ -62,20 +72,27 @@ def create_geojson(detections, dets_table='', inc=5000):
     # Remove any release locations
     dets = dets[~dets['startunqdetecid'].astype(str).str.contains("release")]
 
+    # Generate the table name from the file, if needed
     if dets_table == '':
         dets_table = full_path_detections.lower().replace('_compressed_detections', '').replace(DATADIRECTORY, '').replace('.csv', '')
 
+    # Get a list of the unique stations
     locs = get_station_locations(dets.station.unique().astype(str), dets_table)
     locs = locs.drop_duplicates(subset='station')
 
+    # Add the station location to the compressed detections
     data = pd.merge(locs, dets, on='station', how='inner')
 
+    # Convert startdate and enddate to a milliseconds
     data['startdate'] = data['startdate'].map(unix_time_millis)
     data['enddate'] = data['enddate'].map(unix_time_millis)
+
+    # Sort by start date and reset the index to 1
     data = data.sort_values(by='startdate', ascending=True)
     data.reset_index(drop=True, inplace=True)
     data.index += 1
 
+    # Create a hue index for each individual catalognumber and return a dictionary
     hue_increment = 360/data.catalognumber.unique().size
     animals = pd.DataFrame(data.catalognumber.unique())
     animals['hue'] = animals.index*hue_increment
@@ -84,12 +101,17 @@ def create_geojson(detections, dets_table='', inc=5000):
     animals = animals.T.to_dict()
 
     detection_geojson = []
+
+    # Get center points fro the map
     center_y = data.latitude.median()
     center_x = data.longitude.median()
+
+
     start = 1
     end = inc
     cap = 100000
 
+    # Loop through the detections and create sets of geojson detections the size of the increment
     while start < data.index.size and start <= cap:
         geojson ={
             "type": "FeatureCollection",
@@ -107,20 +129,25 @@ def create_geojson(detections, dets_table='', inc=5000):
                     'hue': str(animals[d['catalognumber']]['hue'])
                 } for index, d in data[start:end].iterrows()]
         }
+        # Add the set of detections to the list
         detection_geojson.append(geojson)
         start = end+1
-        end = end + inc
+        end += inc
 
+    # Print message if there are more than cap for detections, defaulting to 100000
     if start > cap:
         print "Only first "+str(cap)+" detections used, please subset your data to see more."
 
+    # Write the geojson out to a jason file
     json_name = full_path_detections.lower().replace('.csv', '').replace('data/', 'data/html/')
     print "Writing JSON file to " +json_name+".json"
     output = open(json_name+".json", 'w')
     json.dump(detection_geojson, output)
     output.close()
 
+    # Create string with just the file name and no path
     filename = json_name.replace(DATADIRECTORY+"html/", '')+".json"
 
+    # Return the json object, filename, and the center points
     return {'json': detection_geojson, 'filename': filename, 'center_x': center_x, 'center_y': center_y}
 
