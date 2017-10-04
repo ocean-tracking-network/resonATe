8f8d77c651be6e6b0cbff9b3ea39bdfd28b4dac7
Alex Nunes <alex.et.nunes@gmail.com>
2017-10-04 11:06:41 -0300

    Minor build changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('README') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -11,13 +11,14 @@ with open('LICENSE') as f:
 
 setup(
     name='resonate',
-    version='0.1.0',
+    version='0.1',
     description='resonate data analysis package',
     long_description=readme,
     author='Alex Nunes',
     include_package_data=True,
     author_email='anunes@dal.ca',
-    url='https://oceantrackingnetwork.org/',
+    url='https://gitlab.oceantrack.org/otndc/resonate',
+    download_url = 'https://gitlab.oceantrack.org/otndc/resonate/archive/0.1.tar.gz',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
