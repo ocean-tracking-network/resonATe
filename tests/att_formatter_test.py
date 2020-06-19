dcae1ac7de5d332f6ffe3899b501611db71d65df
Ryan Gosse <ryan.gosse@dal.ca>
2020-06-19 15:59:19 -0300

    Added test for ATT formatter, and changed version

---
+++ tests/att_formatter_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import pandas as pd
+import pandas.testing as pt
+from colorama import Fore as c
+import pickle
+from resonate.att_formatter import create_att_dictionary_format
+
+
+class CohortTest(unittest.TestCase):
+
+    def test_att_formatter(self):
+        print(c.YELLOW + 'Testing ATT Formatter...' + c.RESET)
+        dets = "tests/assertion_files/nsbs_2014_short.csv"
+        tags = "tests/assertion_files/nsbs_tag_metadata.xls"
+        deployments = "tests/assertion_files/hfx_deployments.xlsx"
+
+        att = create_att_dictionary_format(dets,
+                                     tags,
+                                     deployments,)
+        with open("tests/assertion_files/att_archive.pkl", 'rb') as f:
+            att_archive = pickle.load(f)
+        pt.assert_frame_equal(att['tag_detections'], att_archive['tag_detections'])
+        pt.assert_frame_equal(att['tag_metadata'], att_archive['tag_metadata'])
+        pt.assert_frame_equal(att['station_information'], att_archive['station_information'])
+        print(c.GREEN + 'OK!\n' + c.RESET)
+
+if __name__ == '__main__':
+    unittest.main()
