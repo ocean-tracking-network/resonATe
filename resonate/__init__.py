b190d7933b7635307d28d71fe92d18bd634577fb
Ryan Gosse <ryan.gosse@dal.ca>
2023-08-02 13:58:56 -0300

    Updated version to 1.1

--- resonate/__init__.py
+++ resonate/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Alex Nunes"
 __credits__ = ["Alex Nunes", "Jon Pye", "Brian Jones", "Marta Mihoff", "Ryan Gosse"]
 __license__ = "GPL"
-__version__ = "1.0.9"
+__version__ = "1.1"
 __maintainer__ = "Ryan Gosse"
 __email__ = "ryan.gosse@dal.ca"
 __status__ = "Production"
